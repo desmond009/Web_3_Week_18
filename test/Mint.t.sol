84c38d2109d5905c2ef8ad8ce2f24f6789061161
desmond009 <ragnarviju2004@gmail.com>
2025-09-15 15:02:36 +0530

    Transfer test_Pass

--- test/Mint.t.sol
+++ test/Mint.t.sol
@@ -16,4 +16,20 @@ contract TestContract is Test {
         assertEq(m.balanceOf(address(this)), 100, "OK");
         assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7),0 , "OK");
     }
+
+    function testTransfer() public{
+        m.mint(address(this), 100);
+        m.transfer(0x7361360D60BE09274EccfebAb510753cA894a7d7, 50);
+
+        assertEq(m.balanceOf(address(this)), 50, "OK");
+        assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7), 50, "OK");
+
+        // Now we change the owner
+        vm.prank(0x7361360D60BE09274EccfebAb510753cA894a7d7);
+        // Now transfer from the new owner to the previous owner
+        m.transfer(address(this), 50);
+
+        assertEq(m.balanceOf(address(this)), 100, "OK");
+        assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7), 0, "OK");
+    }
 }
