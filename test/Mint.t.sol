76e0d94b0bd175a5474a8702b123a04c108f81f7
desmond009 <ragnarviju2004@gmail.com>
2025-09-15 15:51:34 +0530

    approval test

--- test/Mint.t.sol
+++ test/Mint.t.sol
@@ -11,12 +11,15 @@ contract TestContract is Test {
         m = new Mint_contract();
     }
 
+
+    // Mint
     function testMint() public {
         m.mint(address(this), 100);
         assertEq(m.balanceOf(address(this)), 100, "OK");
         assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7),0 , "OK");
     }
 
+    // Transfer
     function testTransfer() public{
         m.mint(address(this), 100);
         m.transfer(0x7361360D60BE09274EccfebAb510753cA894a7d7, 50);
@@ -31,5 +34,22 @@ contract TestContract is Test {
 
         assertEq(m.balanceOf(address(this)), 100, "OK");
         assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7), 0, "OK");
+    }
+
+    // Approval
+    function testApproval() public {
+        m.mint(address(this), 100);
+
+        // This new address has the permission to spend upto 10 tokens of the contract (address(this))
+        m.approve(0x7361360D60BE09274EccfebAb510753cA894a7d7, 10);
+
+        // Change the owner......0x7361360D60BE09274EccfebAb510753cA894a7d7 is the new owner 
+        vm.prank(0x7361360D60BE09274EccfebAb510753cA894a7d7);
+
+        // Now transfer from the new owner to the previous owner
+        m.transferFrom(address(this), 0x7361360D60BE09274EccfebAb510753cA894a7d7, 5);
+
+        assertEq(m.balanceOf(address(this)), 95 ,"OK");
+        assertEq(m.balanceOf(0x7361360D60BE09274EccfebAb510753cA894a7d7), 5, "OK");
     }
 }
